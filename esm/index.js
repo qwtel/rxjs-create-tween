da4afe17ebe7840e2743d56df7810d0443850811
Florian Klampfer <mail@qwtel.com>
2018-03-15 12:14:55 +0000

    add es module version

---
+++ esm/index.js
@@ -0,0 +1,40 @@
+import { Observable } from 'rxjs/_esm5/Observable'; // eslint-disable-line
+
+/**
+ * Creates an observable that emits samples from an easing function on every animation frame
+ * for a duration `d` ms.
+ *
+ * The first value will be emitted on the next animation frame,
+ * and is the value of the easing function at `t = 0`.
+ * The final value is guaranteed to be the easing function at `t = d`.
+ * The observable completes one frame after the final value was emitted.
+ *
+ * @param {function(t: number, b: number, c: number, d: number, [s]: number): number} easingFunction
+ * - the easing fuction to sample from; can use any of Robert Penner's easing functions
+     (without the `x` paramter)
+ * @param {number} b - beginning value and 2nd parameter of the easing function
+ * @param {number} c - change in value (or end value) and 3rd parameter of the easing function
+ * @param {number} d - total duration of the tween in ms and 4th parameter of the easing function
+ * @param {number} [s] - 5th parameter of the easing function (optional)
+ * @return {Observable<number>} - an observable emitting samples of the easing function on
+ *   animation frames for `d` ms.
+ */
+export function createTween(easingFunction, b, c, d, s) {
+  return Observable.create((observer) => {
+    let startTime;
+    let id = requestAnimationFrame(function sample(time) {
+      startTime = startTime || time;
+      const t = time - startTime;
+      if (t < d) {
+        observer.next(easingFunction(t, b, c, d, s));
+        id = requestAnimationFrame(sample);
+      } else {
+        observer.next(easingFunction(d, b, c, d, s));
+        id = requestAnimationFrame(() => observer.complete());
+      }
+    });
+    return () => { if (id) { cancelAnimationFrame(id); } };
+  });
+}
+
+export default createTween;
