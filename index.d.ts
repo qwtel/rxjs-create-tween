113127de23c4c179e0cb9858847beb3305606f80
petervaro <hello@petervaro.com>
2019-03-14 15:56:28 +0000

    declaration file formatted properly

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,19 @@
-declare module 'rxjs-create-tween' {
-    import {Observable} from 'rxjs';
+declare module "rxjs-create-tween" {
+  import { Observable } from "rxjs";
 
-    export type EasingFunction = (
-        currentTime: number,
-        beginValue: number,
-        endValue: number,
-        totalDuration: number,
-        s?: number,
-    ) => number;
+  export type EasingFunction = (
+    currentTime: number,
+    beginValue: number,
+    endValue: number,
+    totalDuration: number,
+    s?: number
+  ) => number;
 
-    export function createTween(
-        easingFunction: EasingFunction,
-        beginValue: number,
-        endValue: number,
-        totalDuration: number,
-        s?: number,
-    ): Observable<number>;
+  export function createTween(
+    easingFunction: EasingFunction,
+    beginValue: number,
+    endValue: number,
+    totalDuration: number,
+    s?: number
+  ): Observable<number>;
 }
